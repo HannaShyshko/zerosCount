9f2d529806c7aa62f8d95460839223cbadf94941
Hanna <hannashishko@gmail.com>
2018-03-02 10:35:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+module.exports = function getZerosCount(number) {
+  // your implementation
+    var five = 5;
+    var result = 0; 
+	while(number > five){
+		result = result + Math.floor(number / five);
+		five *= 5;
+	}
+	return result;
+}
 
